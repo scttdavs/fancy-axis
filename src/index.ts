c033e068f5490cd701e0de4052d76f22bc4cecd9
Scott Davis <sc.ttdav.s@gmail.com>
2020-03-31 13:30:35 -0400

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,37 @@
+// import { selectAll } from "d3-selection";
+// import { Axis } from "d3-axis";
+// import { max } from "d3-array";
 
+// # axis.tickPadding([padding]) <>
+// If padding is specified, sets the padding to the specified value in pixels and returns the axis. If padding is not specified, returns the current padding which defaults to 3 pixels.
 
-const fancy = () => {
+// 1. update padding on axis with max widths/values
+// 2. draw dots (circles)
+// 3. config?
 
-};
+// axis: axis you want to make fancy
+// data: array of only the data you want to make fancy for that axis
+// const fancy = (axis: Axis<any>, data: any[]) => {
+    // const ticks = selectAll(tickSelector);
+
+    // const scale = axis.scale();
+    // const maxValue = max(data);
+    // ticks
+    //     //in the rest of the method calls:
+    //     .select('line') //grab the tick line
+    //     .attr('class', 'quadrantBorder') //style with a custom class and CSS
+    //     .style('stroke-width', 5);
+
+    
+// };
+
 
 const stripChart = () => {
-
+    // axis.tickPadding()
+    return 42;
 };
 
 export default {
     stripChart,
-    fancy,
+    // fancy,
 };
--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,13 @@ const path = require('path');
 module.exports = {
     entry: './src/index.ts',
     module: {
-    rules: [
-        {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        },
-    ],
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
     },
     resolve: {
         extensions: [ '.tsx', '.ts', '.js' ],
