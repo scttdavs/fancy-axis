d292368c516570b8da43a550e5a6215c81d80730
Scott Davis <sc.ttdav.s@gmail.com>
2020-04-02 14:55:54 -0400

    add render test

--- tests/rug-plot.test.ts
+++ tests/rug-plot.test.ts
@@ -1,7 +1,18 @@
+import fs from "fs";
+import path from "path";
 import { rugPlot } from "../src/index";
+import { JSDOM } from "jsdom";
+import { select } from "d3-selection";
 import { scaleLinear } from "d3-scale";
+import { axisLeft } from "d3-axis";
+
+const file = (name: string) => fs.readFileSync(path.join(__dirname, name), "utf8").replace(/\n\s*/mg, "")
 
 describe("rugPlot", () => {
+    afterEach(() => {
+        document.body.innerHTML = "";
+    });
+
     it("proxies through to the underlying d3 axis instnace", () => {
         // sets the tick arguments
         const axis = rugPlot.axisLeft(scaleLinear()).ticks(20);
@@ -11,5 +22,13 @@ describe("rugPlot", () => {
         // @ts-ignore
         axis.ticks();
         expect(axis.tickArguments()).toEqual([]);
+    });
+
+    it("produces the correct output", () => {
+        var bodyExpected = (new JSDOM(file("rug-plot-axis.html"))).window.document.body;
+        document.body.innerHTML = "<!DOCTYPE html><svg><g></g></svg>"
+        // @ts-ignore
+        select(document.body).select("g").call(rugPlot.axisLeft(scaleLinear().range([0, 500])));
+        expect(document.body.outerHTML).toEqual(bodyExpected.outerHTML);
     });
 });
